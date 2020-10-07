7ff0720d73035193f47e7941f1364b5e80ae11d1
이재빈 <woqls226@gmail.com>
2020-10-08 08:09:02 +0900

    Core Cloud 수신 모듈 작성

--- Modules/core_cloud.py
+++ Modules/core_cloud.py
@@ -4,7 +4,7 @@ import numpy as np
 from multiprocessing import Queue
 
 from labeling_module import LabelingModule
-#socket에서 수신한 버퍼를 반환함.
+#socket에서 수신한 버퍼를 반환
 def recvall(sock, count):
     # 바이트 문자열
     buf = b''
@@ -25,22 +25,22 @@ if __name__ == "__main__":
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     print('Socket created')
     
-    #서버의 아이피와 포트번호 지정
+    #CoreCloud IP, PortNumber set
     s.bind((HOST,PORT))
     print('Socket bind complete')
-    # 클라이언트의 접속을 기다린다. (클라이언트 연결을 10개까지 받는다)
+    # Edge Cloud 접속wait (클라이언트 연결을 10개까지 받는다)
     s.listen(10)
     print('Socket now listening')
     
-    #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
+    #연결, conn 소켓 객체, addr socket binded addr
     conn,addr=s.accept()
     while True:
-        # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
+        # client에서 받은 stringData length (==(str(len(stringData))).encode().ljust(16))
         length = recvall(conn, 16)
         stringData = recvall(conn, int(length))
         data = np.fromstring(stringData, dtype = 'uint8')
         
-        #data를 디코딩한다.
+        #data decode
         cropped = cv2.imdecode(data, cv2.IMREAD_COLOR)
         cropped = cv2.resize(cropped, (48,48)) #Crop Image Resize
         lm.new_tensor(cropped) # Predict result
